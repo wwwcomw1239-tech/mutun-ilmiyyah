cfa229fb7540c9ca2e19694220601c25908c2924
Z User <z@container>
2026-03-19 16:15:00 +0000

    Deploy: Fix download and UI issues

---
+++ _next/static/klEh0oO9tjlXU2_BBjs0X/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mutun-ilmiyyah//_next/:path+",
+        "destination": "/mutun-ilmiyyah/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
